560bc8fb42bc53d145eb77bab5d95c16e4a37459
faith <faithjacks63@gmail.com>
2019-07-21 11:50:51 -0400

    add on click on the what to do section to enable the image to disappear on click

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-  $("#design img").click(function(){
+  $("#design").on("click",function(){
     $("#design p"). toggle();
+  },function(){
+      $("#design img"). toggle();
     });
-  $("#dev img").click(function(){
+  $("#dev").on("click",function(){
     $("#dev p"). toggle();
+  },function(){
+      $("#dev img"). toggle();
     });
-  $("#product img").click(function(){
+  $("#product").on("click",function(){
     $("#product p"). toggle();
+  },function(){
+      $("#product img"). toggle();
+  });
     });
-});
