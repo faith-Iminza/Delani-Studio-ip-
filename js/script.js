a0a20789160771accfb3200f8a70333f2e893a24
faith <faithjacks63@gmail.com>
2019-07-22 00:44:42 -0400

    add the hover effect on the portfolio section

--- js/script.js
+++ js/script.js
@@ -1,17 +1,74 @@
 $(document).ready(function(){
-  $("#design").on("click",function(){
-    $("#design p"). toggle();
-  },function(){
-      $("#design img"). toggle();
-    });
-  $("#dev").on("click",function(){
-    $("#dev p"). toggle();
-  },function(){
-      $("#dev img"). toggle();
-    });
-  $("#product").on("click",function(){
-    $("#product p"). toggle();
-  },function(){
-      $("#product img"). toggle();
-  });
-    });
+ $("#design").on("click",function(){
+   $("#design p"). toggle();
+ },function(){
+     $("#design img"). toggle();
+   });
+ $("#dev").on("click",function(){
+   $("#dev p"). toggle();
+ },function(){
+     $("#dev img"). toggle();
+   });
+ $("#product").on("click",function(){
+   $("#product p"). toggle();
+ },function(){
+     $("#product img"). toggle();
+ });
+   });
+ //hover effect:
+ $(document).ready(function(){
+   $("#portimg").mouseover(function(){
+     $("#porttext").show();
+   }).mouseout(function(){
+     $("#porttext").hide();
+   });
+ });
+ $(document).ready(function(){
+   $("#portimg1").mouseover(function(){
+     $("#porttext1").show();
+   }).mouseout(function(){
+     $("#porttext1").hide();
+   });
+ });
+ $(document).ready(function(){
+   $("#portimg2").mouseover(function(){
+     $("#porttext2").show();
+   }).mouseout(function(){
+     $("#porttext2").hide();
+   });
+ });
+ $(document).ready(function(){
+   $("#portimg3").mouseover(function(){
+     $("#porttext3").show();
+   }).mouseout(function(){
+     $("#porttext3").hide();
+   });
+ });
+ $(document).ready(function(){
+   $("#portimg4").mouseover(function(){
+     $("#porttext4").show();
+   }).mouseout(function(){
+     $("#porttext4").hide();
+   });
+ });
+ $(document).ready(function(){
+   $("#portimg5").mouseover(function(){
+     $("#porttext5").show();
+   }).mouseout(function(){
+     $("#porttext5").hide();
+   });
+ });
+ $(document).ready(function(){
+   $("#portimg6").mouseover(function(){
+     $("#porttext6").show();
+   }).mouseout(function(){
+     $("#porttext6").hide();
+   });
+ });
+ $(document).ready(function(){
+   $("#portimg7").mouseover(function(){
+     $("#porttext7").show();
+   }).mouseout(function(){
+     $("#porttext7").hide();
+   });
+ });
